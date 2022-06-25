adb70d61a0a9f7820149852cc98403144c7ed447
Kağan KAHRAMAN <kaane.kahramane@gmail.com>
2022-06-25 11:36:51 +0300

    Update AuthenticateGuest.php

--- app/Http/Middleware/AuthenticateGuest.php
+++ app/Http/Middleware/AuthenticateGuest.php
@@ -47,7 +47,7 @@ class AuthenticateGuest
                 $agent = new Agent();
                 $agent->setUserAgent($request->userAgent());
                 if ($agent->isRobot()) return false;
-                Log::emergency($host;
+                Log::emergency($host);
     
                 if (Str::contains(parse_url($guest->website->url, PHP_URL_HOST), $host)) {
                     \Auth::login($guest);
